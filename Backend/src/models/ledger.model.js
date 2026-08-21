ff06506d80378948d8628041a0b3d2d37161880f
Pratik Lachwani <pratiklachwani7@gmail.com>
2026-08-22 00:33:24 +0530

    Ledger Model Created

--- Backend/src/models/ledger.model.js
+++ Backend/src/models/ledger.model.js
@@ -34,3 +34,15 @@ function preventLedgerModification()
 {
     throw new Error("Ledger entries are immutable and cannot be modified or deleted") ;
 }
+
+ledgerSchema.pre("findOneAndUpdate",preventLedgerModification);
+ledgerSchema.pre("updateOne",preventLedgerModification);
+ledgerSchema.pre("deleteOne",preventLedgerModification);
+ledgerSchema.pre("remove",preventLedgerModification);
+ledgerSchema.pre("deleteMany",preventLedgerModification);
+ledgerSchema.pre("updateMany",preventLedgerModification);
+ledgerSchema.pre("findOneAndDelete",preventLedgerModification);
+ledgerSchema.pre("findOneAndReplace",preventLedgerModification);
+
+const ledgerModel = mongoose.model("ledger",ledgerSchema) ;
+module.exports = ledgerModel ;
